7254346f931ecbf68e44a746a9524215f32e8e21
lamcc0603 <chungchilam12@gmail.com>
2022-10-13 16:28:45 +0700

    làm phần course quiz

---
+++ src/pages/Course/QuizItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="quizItem">
+    <h3>{{ title }}</h3>
+    <span class="countdown">{{ timer }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      require,
+      Type: String,
+    },
+    deadline: {
+      require,
+      Type: Date,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
